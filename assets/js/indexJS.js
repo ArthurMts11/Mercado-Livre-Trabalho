e9b059e80a918819c897f56ddc0159b1c5dc2d94
Arthur <arthurmtsmts@gmail.com>
2024-12-10 23:32:38 -0300

    v0.2

--- assets/js/indexJS.js
+++ assets/js/indexJS.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
     $.getJSON("https://fakestoreapi.com/products", function(products) {
         const productList = $('#productList'); // Contêiner onde os produtos serão listados
 
-        // Pegar os dois primeiros produtos
-        const firstTwoProducts = products.slice(0, 2);
+        // Pegar os dois primeiros produtos (ou o número desejado de produtos)
+        const firstTwoProducts = products.slice(0, 38);
 
         firstTwoProducts.forEach(product => {
             // Formatar o preço antigo e o preço novo
             const formattedPrice = (product.price).toFixed(2);
             const discountPercentage = 14; // Desconto fictício de 14%
             const discountPrice = (product.price * (1 - discountPercentage / 100)).toFixed(2);
-            
+
             // Adicionar o produto à lista com um link em torno da div inteira
             productList.append(`
-                <a href="/produto/${product.id}" class="produto-link">
+                <a href="produto.html?id=${product.id}" class="produto-link">
                     <div class="produto">
                         <img src="${product.image}" alt="${product.title}">
                         <p class="descricao">${product.title}</p>
